60a5056e4d74bc78c9398b8df9130f912b65b308
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-07-21 23:22:54 +0530

    night commit

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -2,7 +2,7 @@ import * as types from "./actionTypes";
 import axios from "axios";
 const getProductsData = () => (dispatch) => {
     dispatch({type: types.GET_PRODUCTS_REQUEST});
-    axios.get("http://localhost:8080/data")
+    axios.get("http://localhost:8080/products")
     .then((res)=>dispatch({type: types.GET_PRODUCTS_SUCCESS, payload:res.data}))
     .catch((err)=>dispatch({type:types.GET_PRODUCTS_FAILURE,payload:err}));
 };
