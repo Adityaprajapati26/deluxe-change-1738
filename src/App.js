e7fbbf9b9f302ab63aa66eee2583fdff631d061d
sumitranjan <sumitranjan.ind@gmail.com>
2022-07-22 14:42:01 +0530

    app.js main file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+
+import logo from "./logo.svg";
 import "./App.css";
-import MainRoutes2 from "./Pages/MainRoutes2";
-import { Box } from "@chakra-ui/react";
+
+import Announcement from "./Components/Announcement";
+import Navbar from "./Components/Navbar";
+import Categories from "./Components/Categories";
+import HomePage from "./Components/HomePage";
+import Products from "./Components/Product/Products";
+import Shop from "./Components/Feed";
+import Feed from "./Components/Feed";
+
+import Footer from "./Components/Footer/Footer";
+
 function App() {
-  return (
-    <Box w="100%">
-      <MainRoutes2 />
-    </Box>
-  );
-}
+	return (
+		<>
+			<Announcement />
+			<hr />
+			<Navbar />
+			<HomePage />
+			<Products />
+			<br />
+			<br />
+			{/* <Categories /> */}
+			{/* <Feed/> */}
+      <hr />
+      <br />
+      <br />
+			<Footer />
+		</>
+	);
+
+  }
 
 export default App;
