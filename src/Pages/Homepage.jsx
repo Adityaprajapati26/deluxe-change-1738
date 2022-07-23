09b43c3bb68ec8b6a9126c0edc4d4ce6268025b2
Adityaprajapati26 <99543597+Adityaprajapati26@users.noreply.github.com>
2022-07-23 20:46:21 +0530

    deluxe-change-1738

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,51 +1,65 @@
-import { Box, Flex, Image, Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, Image, Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getProductsData, getSortData } from "../Redux/action";
-
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { getProduct} from "../Redux/Appreducer/action";
+import { SimpleGrid } from '@chakra-ui/react'
 const Homepage = () => {
-  const products = useSelector((state) => state.products);
+  const products = useSelector((state)=>state.Appreducer.products);
   const dispatch = useDispatch();
-
-  const handleSort = (e) => {
-    console.log(e.target.value);
-    if (e.target.value === "asc") {
-      const ascData = products.sort((a, b) => a.price - b.price);
-      console.log("clicked", ascData);
-      dispatch(getSortData(ascData));
-    } else if (e.target.value === "desc") {
-      const descData = products.sort((a, b) => b.price - a.price);
-      dispatch(getSortData(descData));
-      console.log(descData, "desc");
-    }
-  };
+  const[sort,setSort]=useState("asc")
+  const navigate=useNavigate()
+const handleSort=(e)=>{
+  setSort(e.target.value)
+}
+console.log(sort)
 
   useEffect(() => {
-    dispatch(getProductsData());
-  }, []);
-  console.log(products, "products");
+    if(products.length===0)
+    {
+      dispatch(getProduct());
+    }
+    
+  }, [dispatch]);
+  const handleClick=()=>{
+    
+  }
+  console.log(products);
   return (
-    <Box>
+    <Box margin={"60px"}>
       <Flex>
         <Text fontWeight="700" paddingRight="1rem">
           Sort by Price
         </Text>
-        <RadioGroup onClick={handleSort}>
+        <RadioGroup value={sort} onClick={(e)=>handleSort(e)}>
           <Stack direction="row">
             <Radio value="asc">Low to High</Radio>
             <Radio value="desc">High to Low</Radio>
           </Stack>
         </RadioGroup>
       </Flex>
-
-      {products.map((items,ind) => (
-        <Box key={ind}>
-        <Image src={items.image}/>
+      <SimpleGrid marginTop={"25px"} columns={[2, 4]} spacing='10px'  >
+      {products.sort((a,b)=>{
+        if(sort=="asc")
+        {
+          return a.Price-b.Price
+        }
+        else if(sort=="desc")
+        {
+          return b.Price-a.Price
+        }
+        else{
+          return 0
+        }
+      }).map((items,ind) => (
+        <Box  key={ind}height="400px">
+       <Link to={`/Product/${items.id}`}><Image w="200px" h="300px"  src={items.image}/></Link>
         <Text>{items.title}</Text>
         <Text>{items.Price}</Text>
         </Box>
       ))}
+      </SimpleGrid>
 
     </Box>
   );
--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -5,17 +5,18 @@ import Login from "./Login";
 import Signup from "./Signup";
 import ReqAuth from "../ReqAuth/ReqAuth";
 import Homepage from "./Homepage";
-
+import Product from "../component/Product"
 const MainRoutes = () => {
   return (
     <Routes>
      
       <Route path="/" element={<Home/>} />
-      <Route path="/" element={< Homepage/>} />
+      <Route path="/New" element={< Homepage/>} />
     {/* //   <Route path="/" element={<Cart/>} /> */}
     <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/checkout" element={<ReqAuth><Checkout/></ReqAuth>} />
+      <Route path="/Product/:id" element={<Product/>} />
        {/* <Route path="/" element={< />} /> */}
 
 
