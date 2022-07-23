7e51da32a1de761c5d448137ef82eb5ab956edd5
Aditya Prajapati <99543597+Adityaprajapati26@users.noreply.github.com>
2022-07-23 17:52:24 +0530

    Checkoutpage

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,7 +1,15 @@
-
 import React, { useReducer } from 'react'
-import { Avatar, Box,Button,Center,Checkbox,Divider,Flex,FormControl,FormLabel,Heading,HStack,Image, Input, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverTrigger, Select, Stack, Text, WrapItem } from '@chakra-ui/react'
+import { Avatar, Box,Button,Center,Checkbox,Divider,Flex,FormControl,FormLabel,Heading,HStack,Image, Input, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverTrigger, Select, Stack, Text, useDisclosure, WrapItem } from '@chakra-ui/react'
 import {ChevronRightIcon} from "@chakra-ui/icons"
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 import {useNavigate} from "react-router-dom"
 function reducer(state,action){
   switch(action.type){
@@ -59,42 +67,61 @@ const Checkout = () => {
   const[state,Setstate]=useReducer(reducer,initstate)
   // const navigate=useNavigate()
   // const data=useSelector()
+  // const data=useSelector((state)=>state.handlecart)
 
-
+  const OverlayTwo = () => (
+    <ModalOverlay
+      bg='none'
+      backdropFilter='auto'
+      backdropInvert='80%'
+      backdropBlur='2px'
+    />
+  )
+  const {  isOpen,onOpen,onClose} = useDisclosure()
+  const [overlay, setOverlay] = React.useState(<OverlayTwo />)
+//   const onClose=()=>{
+    
+//     setTimeout(()=>{
+//       console.log("Hello")
+//        },3000)
+      
+    
+//  }
+  
   return (
-    <Box border={"1px solid black"} display="flex">
+    <Box  display="flex">
         
       
           
-        <Box margin="10vh" w="50%" border={"1px solid green"} padding="5px">
+        <Box margin="10vh" w="50%"  padding="5px">
            <Image  h="80px"  src="https://cdn.shopify.com/s/files/1/0627/7388/7215/files/04122019_logo2.png?v=1645644264" />
            <Flex display="flex">
            <Center h="20px"><Text fontSize={"10px"}>Cart<ChevronRightIcon boxSize={"15px"}/> Information<ChevronRightIcon boxSize={"15px"}/>Shipping<ChevronRightIcon boxSize={"15px"}/>Payment</Text></Center>
            </Flex>
-           <Box h={"100px"} w="90%" border={"1px solid black"}  borderRadius={"5px"}>
+           <Box h={"100px"} w="90%"   borderRadius={"5px"}>
             <Text align={"center"}>Express Checkout</Text>
             <Box   margin="auto" w="100%" h="10vh">
                 <Flex justifyContent={"space-around"} alignItems="center" paddingTop={"10px"}> 
-                <Box h="50px" w="180px"  border={"1px solid black"} borderRadius={"5px"}><Image h="48px" w="178px" src='https://www.pagetraffic.com/blog/wp-content/uploads/2022/05/about-shop-pay-1536x806.jpg'/></Box>
-                <Box  h="50px" w="180px" border={"1px solid black"} borderRadius={"5px"}><Image  h="48px" w="178px"src='https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_111x69.jpg'/></Box>
-                <Box  h="50px" w="180px" border={"1px solid black"} align="center" borderRadius={"5px"}> <Image h="45px" w="178px"  src='https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/articleimages/2022/03/30/google-pay-crs-1-1096059-1648632689.jpg?itok=Nt6gBe2W'/> </Box>
+                <Box h="50px" w="180px"   borderRadius={"5px"}><Image h="48px" w="178px" src='https://www.pagetraffic.com/blog/wp-content/uploads/2022/05/about-shop-pay-1536x806.jpg'/></Box>
+                <Box  h="50px" w="180px"  borderRadius={"5px"}><Image  h="48px" w="178px"src='https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_111x69.jpg'/></Box>
+                <Box  h="50px" w="180px"  align="center" borderRadius={"5px"}> <Image h="45px" w="178px"  src='https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/articleimages/2022/03/30/google-pay-crs-1-1096059-1648632689.jpg?itok=Nt6gBe2W'/> </Box>
                 </Flex>
              </Box>
             </Box>
-            <Box h="100px" w="400px" border={"1px solid black"}>
+            <Box  w="400px" >
                 <Text>Contack Information</Text>
                 <Flex justifyContent={"space-around"}> 
                 <WrapItem marginTop={"10px"}>
                <Avatar size='md'  />
                </WrapItem>
-               <Box w="80%" border={"1px solid black"}>
+               <Box w="80%" >
                   <Text>name(id)</Text>
                   <Button h="25px">Logout</Button>
                </Box>
                 </Flex>
                 <Checkbox>Email Me with new and Offers</Checkbox>
                </Box>
-              <Box  marginTop={"10px"} border={"1px solid black"} h="500px" w="100%" gap={"30px"} >
+              <Box  marginTop={"10px"}  h="500px" w="100%" gap={"30px"} >
                 <Text size={"25px"}>Shipping Address</Text>
                 <Stack marginTop={"20px"} spacing="15px">
                 <FormControl  >
@@ -160,14 +187,22 @@ const Checkout = () => {
               </Box>
              <Box display={"flex"} justifyContent="space-between" margin="15px">
               <Text>Return to Shoping</Text>
-              <Button bg="black" color={"white"}>Pay</Button>
+              <Button
+                  ml='4'
+                    onClick={() => {
+                   setOverlay(<OverlayTwo />)
+                    onOpen()
+                        }}
+                   >
+               Pay
+                      </Button>
              </Box>
            
         </Box>
              
         
 
-             <Box marginTop={"10vh"} width="50%" marginRight={"15px"}  border={"1px solid green"}>
+             <Box marginTop={"10vh"} width="50%" marginRight={"15px"}  >
            
               {/* Map the Data  with useSelector*/}
               {/* <Image src="" border="1px solid red" w="150px" h="150px"/> */}
@@ -186,16 +221,28 @@ const Checkout = () => {
 
               
              </Box>
+
+         {/* model */}
+         <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        {overlay}
+        <ModalContent>
+          <ModalHeader>Payment is Succesfull</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Amount</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={()=>onClose()}>Back to Shopping</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+         {/* model */}
+
     </Box>
   )
 }
 
 export default Checkout
-
-
-
-
-
 
 
 
